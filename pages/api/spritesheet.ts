9389ea930b91f01ff069ef3449b37e267dcd5260
Nelson Rodrigues <nelsonrodrigues87@outlook.com>
2022-08-30 00:25:27 +0100

    Preserve original filename

--- pages/api/spritesheet.ts
+++ pages/api/spritesheet.ts
@@ -20,7 +20,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     try {
         const { files }: FormidableFormData = await new Promise((resolve, reject) => { 
             const form = formidable({
-                keepExtensions: true
+                keepExtensions: true,
+                filename: (name, ext) => name + ext 
             });
 
             form.parse(req, async (err, fields, files) => {
